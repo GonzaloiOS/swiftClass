724ce9746c8cf10379654f0b2963f16f2054347f
Gonzalo Linares N <glinares1408@gmail.com>
2016-03-20 19:57:57 -0500

    Processing dictionary from open library.

--- ConsultaSwift/ConsultaSwift/ViewController.swift
+++ ConsultaSwift/ConsultaSwift/ViewController.swift
@@ -17,12 +17,26 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     
+    @IBOutlet weak var titleBookTextView: UITextView!
+    
+    @IBOutlet weak var authorTextView: UITextView!
+    
+    
+    @IBOutlet weak var portraitTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //Delegate
+        
         self.searchBookTextField.delegate = self
         self.resultTextView.userInteractionEnabled = false
+        //self.resultTextView.hidden = true
+        
+        self.titleBookTextView.userInteractionEnabled = false
+        self.authorTextView.userInteractionEnabled = false
+        self.portraitTextView.editable = false
+        
         self.activityIndicator.hidesWhenStopped = true
         self.activityIndicator.hidden = true
     }
@@ -67,6 +81,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
             //nill means no error
             
             var responseToShow:String = ""
+            var titleBook:String = ""
+            var portraitBook:String = ""
+            var authorName:String = ""
+            
             
             if((error) != nil){
                 
@@ -77,12 +95,45 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 let rawResponse:String? = String(data: data!, encoding: NSUTF8StringEncoding)
                 
                 responseToShow = "Resultado de la búsqueda: " + rawResponse!
+                
+                if(!(rawResponse! == "{}")){
+                    
+                    //processing JSON, I have to uwe tray and catch
+                    
+                    do{
+                        
+                        let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves)
+                        
+                        let jsonDictionary = json as! NSDictionary
+                        
+                        //let isbnDictionary = jsonDictionary["ISBN:978-84-376-0494-7"] as! NSDictionary
+                        let isbnDictionary = jsonDictionary["ISBN:"+text] as! NSDictionary
+                        
+                        titleBook = isbnDictionary["title"] as! NSString as String
+                        
+                        portraitBook = isbnDictionary["url"] as! NSString as String
+                        
+                        let authorsArray = isbnDictionary["authors"] as! NSArray
+                        
+                        //authorName = subjectPeopleDictionary["name"] as! NSString as String
+                        let authorsDictionary = authorsArray[0] as! NSDictionary
+                        
+                        authorName = authorsDictionary["name"] as! NSString as String
+                        
+                    }catch _{
+                        
+                    }
+                }
             }
             
             dispatch_async(dispatch_get_main_queue(),{
                 //show result
                 self.activityIndicator.stopAnimating()
                 self.resultTextView.text = responseToShow
+                
+                self.titleBookTextView.text = titleBook
+                self.authorTextView.text = authorName
+                self.portraitTextView.text = portraitBook
             })
             
         }
